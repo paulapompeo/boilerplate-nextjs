4bbd55faebcbdc45146018f4a301dc316ca9fec4
paulapompeo <paulapompeoc@gmail.com>
2021-11-02 20:21:17 +0100

    adds first components and styles

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,24 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>bino</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React avançado escrito ao lado"
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJs, e Styled-Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="desenvolvador de frente para tela com código"
+    />
+  </S.Wrapper>
 )
 
 export default Main
+
+// https://github.com/React-Avancado/landing-page/tree/master/src/images
+
+// https://github.com/React-Avancado/landing-page/tree/master/public/img
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -5,8 +5,16 @@ describe('<Main />', () => {
   it('should render the heading', () => {
     const { container } = render(<Main />)
 
-    expect(screen.getByRole('heading', { name: /bino/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /React Avançado/i })
+    ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
